48d181b4d86279e7a16752d8a063908732544f4b
Kamigami55 <eason@easonchang.com>
2022-02-07 00:00:38 +0800

    Migrate post page from tailwind template
    https://github.com/timlrx/tailwind-nextjs-starter-blog

--- src/lib/mdx.js
+++ src/lib/mdx.js
@@ -1,7 +1,12 @@
 import fs from 'fs'
 import matter from 'gray-matter'
+// import Head from 'next/head'
+// import { MDXRemote } from 'next-mdx-remote'
+import { serialize } from 'next-mdx-remote/serialize'
 // import { bundleMDX } from 'mdx-bundler'
 import path from 'path'
+import rehypeSlug from 'rehype-slug'
+import prism from 'remark-prism'
 
 // import readingTime from 'reading-time'
 // import rehypeAutolinkHeadings from 'rehype-autolink-headings'
@@ -24,12 +29,12 @@ import getAllFilesRecursively from './utils/files'
 
 const root = process.cwd()
 
-// export function getFiles(type) {
-//   const prefixPaths = path.join(root, 'data', type)
-//   const files = getAllFilesRecursively(prefixPaths)
-//   // Only want to return blog/path and ignore root, replace is needed to work on Windows
-//   return files.map((file) => file.slice(prefixPaths.length + 1).replace(/\\/g, '/'))
-// }
+export function getFiles() {
+  const prefixPaths = path.join(root, 'posts')
+  const files = getAllFilesRecursively(prefixPaths)
+  // Only want to return blog/path and ignore root, replace is needed to work on Windows
+  return files.map((file) => file.slice(prefixPaths.length + 1).replace(/\\/g, '/'))
+}
 
 export function formatSlug(slug) {
   return slug.replace(/\.(mdx|md)/, '')
@@ -41,74 +46,94 @@ export function dateSortDesc(a, b) {
   return 0
 }
 
-// export async function getFileBySlug(type, slug) {
-//   const mdxPath = path.join(root, 'data', type, `${slug}.mdx`)
-//   const mdPath = path.join(root, 'data', type, `${slug}.md`)
-//   const source = fs.existsSync(mdxPath)
-//     ? fs.readFileSync(mdxPath, 'utf8')
-//     : fs.readFileSync(mdPath, 'utf8')
+export async function getFileBySlug(slug) {
+  const mdxPath = path.join(root, 'posts', `${slug}.mdx`)
+  const mdPath = path.join(root, 'posts', `${slug}.md`)
+  const source = fs.existsSync(mdxPath)
+    ? fs.readFileSync(mdxPath, 'utf8')
+    : fs.readFileSync(mdPath, 'utf8')
 
-//   // https://github.com/kentcdodds/mdx-bundler#nextjs-esbuild-enoent
-//   if (process.platform === 'win32') {
-//     process.env.ESBUILD_BINARY_PATH = path.join(root, 'node_modules', 'esbuild', 'esbuild.exe')
-//   } else {
-//     process.env.ESBUILD_BINARY_PATH = path.join(root, 'node_modules', 'esbuild', 'bin', 'esbuild')
-//   }
+  // https://github.com/kentcdodds/mdx-bundler#nextjs-esbuild-enoent
+  // if (process.platform === 'win32') {
+  //   process.env.ESBUILD_BINARY_PATH = path.join(root, 'node_modules', 'esbuild', 'esbuild.exe')
+  // } else {
+  //   process.env.ESBUILD_BINARY_PATH = path.join(root, 'node_modules', 'esbuild', 'bin', 'esbuild')
+  // }
 
-//   let toc = []
+  // let toc = []
 
-//   // Parsing frontmatter here to pass it in as options to rehype plugin
-//   const { data: frontmatter } = matter(source)
-//   const { code } = await bundleMDX({
-//     source,
-//     // mdx imports can be automatically source from the components directory
-//     cwd: path.join(root, 'components'),
-//     xdmOptions(options) {
-//       // this is the recommended way to add custom remark/rehype plugins:
-//       // The syntax might look weird, but it protects you in case we add/remove
-//       // plugins in the future.
-//       options.remarkPlugins = [
-//         ...(options.remarkPlugins ?? []),
-//         remarkExtractFrontmatter,
-//         [remarkTocHeadings, { exportRef: toc }],
-//         remarkGfm,
-//         remarkCodeTitles,
-//         [remarkFootnotes, { inlineNotes: true }],
-//         remarkMath,
-//         remarkImgToJsx,
-//       ]
-//       options.rehypePlugins = [
-//         ...(options.rehypePlugins ?? []),
-//         rehypeSlug,
-//         rehypeAutolinkHeadings,
-//         rehypeKatex,
-//         [rehypeCitation, { path: path.join(root, 'data') }],
-//         [rehypePrismPlus, { ignoreMissing: true }],
-//         rehypePresetMinify,
-//       ]
-//       return options
-//     },
-//     esbuildOptions: (options) => {
-//       options.loader = {
-//         ...options.loader,
-//         '.js': 'jsx',
-//       }
-//       return options
-//     },
-//   })
+  // const postFilePath = path.join(POSTS_PATH, `${params.slug}.mdx`)
+  // const source = fs.readFileSync(postFilePath)
 
-//   return {
-//     mdxSource: code,
-//     toc,
-//     frontMatter: {
-//       readingTime: readingTime(code),
-//       slug: slug || null,
-//       fileName: fs.existsSync(mdxPath) ? `${slug}.mdx` : `${slug}.md`,
-//       ...frontmatter,
-//       date: frontmatter.date ? new Date(frontmatter.date).toISOString() : null,
-//     },
-//   }
-// }
+  const { content, data: frontmatter } = matter(source)
+  const mdxSource = await serialize(content, {
+    // Optionally pass remark/rehype plugins
+    mdxOptions: {
+      remarkPlugins: [prism],
+      rehypePlugins: [rehypeSlug],
+    },
+    scope: frontmatter,
+  })
+
+  // return {
+  //   props: {
+  //     source: mdxSource,
+  //     frontMatter: data,
+  //   },
+  // }
+
+  // Parsing frontmatter here to pass it in as options to rehype plugin
+  // const { data: frontmatter } = matter(source)
+  // const { code } = await bundleMDX({
+  //   source,
+  //   // mdx imports can be automatically source from the components directory
+  //   cwd: path.join(root, 'components'),
+  //   xdmOptions(options) {
+  //     // this is the recommended way to add custom remark/rehype plugins:
+  //     // The syntax might look weird, but it protects you in case we add/remove
+  //     // plugins in the future.
+  //     options.remarkPlugins = [
+  //       ...(options.remarkPlugins ?? []),
+  //       remarkExtractFrontmatter,
+  //       [remarkTocHeadings, { exportRef: toc }],
+  //       remarkGfm,
+  //       remarkCodeTitles,
+  //       [remarkFootnotes, { inlineNotes: true }],
+  //       remarkMath,
+  //       remarkImgToJsx,
+  //     ]
+  //     options.rehypePlugins = [
+  //       ...(options.rehypePlugins ?? []),
+  //       rehypeSlug,
+  //       rehypeAutolinkHeadings,
+  //       rehypeKatex,
+  //       [rehypeCitation, { path: path.join(root, 'data') }],
+  //       [rehypePrismPlus, { ignoreMissing: true }],
+  //       rehypePresetMinify,
+  //     ]
+  //     return options
+  //   },
+  //   esbuildOptions: (options) => {
+  //     options.loader = {
+  //       ...options.loader,
+  //       '.js': 'jsx',
+  //     }
+  //     return options
+  //   },
+  // })
+
+  return {
+    mdxSource,
+    // toc,
+    frontMatter: {
+      // readingTime: readingTime(code),
+      slug: slug || null,
+      fileName: fs.existsSync(mdxPath) ? `${slug}.mdx` : `${slug}.md`,
+      ...frontmatter,
+      date: frontmatter.date ? new Date(frontmatter.date).toISOString() : null,
+    },
+  }
+}
 
 export async function getAllFilesFrontMatter() {
   const prefixPaths = path.join(root, 'posts')
---
+++ src/pages/posts/[...slug].js
@@ -0,0 +1,100 @@
+// import fs from 'fs'
+
+import { MDXRemote } from 'next-mdx-remote'
+
+// const DEFAULT_LAYOUT = 'PostLayout'
+import {
+  CustomH1,
+  CustomH2,
+  CustomH3,
+  CustomH4,
+  CustomH5,
+  CustomH6,
+} from '@/components/CustomHeading'
+// import { MDXLayoutRenderer } from '@/components/MDXComponents'
+import PageTitle from '@/components/PageTitle'
+import PostLayout from '@/layouts/PostLayout'
+// import generateRss from '@/lib/generate-rss'
+import { formatSlug, getAllFilesFrontMatter, getFileBySlug, getFiles } from '@/lib/mdx'
+
+import CustomLink from '../../components/CustomLink'
+
+// Custom components/renderers to pass to MDX.
+// Since the MDX files aren't loaded by webpack, they have no knowledge of how
+// to handle import statements. Instead, you must include components in scope
+// here.
+const components = {
+  a: CustomLink,
+  h1: CustomH1,
+  h2: CustomH2,
+  h3: CustomH3,
+  h4: CustomH4,
+  h5: CustomH5,
+  h6: CustomH6,
+}
+
+export async function getStaticPaths() {
+  const posts = getFiles()
+  return {
+    paths: posts.map((p) => ({
+      params: {
+        slug: formatSlug(p).split('/'),
+      },
+    })),
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const allPosts = await getAllFilesFrontMatter()
+  const postIndex = allPosts.findIndex((post) => formatSlug(post.slug) === params.slug.join('/'))
+  const prev = allPosts[postIndex + 1] || null
+  const next = allPosts[postIndex - 1] || null
+  const post = await getFileBySlug(params.slug.join('/'))
+  // const authorList = post.frontMatter.authors || ['default']
+  // const authorPromise = authorList.map(async (author) => {
+  //   const authorResults = await getFileBySlug('authors', [author])
+  //   return authorResults.frontMatter
+  // })
+  // const authorDetails = await Promise.all(authorPromise)
+
+  // rss
+  // if (allPosts.length > 0) {
+  //   const rss = generateRss(allPosts)
+  //   fs.writeFileSync('./public/feed.xml', rss)
+  // }
+
+  return { props: { post, prev, next } }
+}
+
+export default function Blog({ post, prev, next }) {
+  const { mdxSource, frontMatter } = post
+
+  return (
+    <>
+      {frontMatter.draft !== true ? (
+        <PostLayout frontMatter={frontMatter} prev={prev} next={next}>
+          <MDXRemote {...mdxSource} components={components} />
+        </PostLayout>
+      ) : (
+        // <MDXLayoutRenderer
+        //   layout={frontMatter.layout || DEFAULT_LAYOUT}
+        //   // toc={toc}
+        //   mdxSource={mdxSource}
+        //   frontMatter={frontMatter}
+        //   // authorDetails={authorDetails}
+        //   prev={prev}
+        //   next={next}
+        // />
+        <div className="mt-24 text-center">
+          <PageTitle>
+            Under Construction{' '}
+            <span role="img" aria-label="roadwork sign">
+              🚧
+            </span>
+          </PageTitle>
+        </div>
+      )}
+    </>
+  )
+}
