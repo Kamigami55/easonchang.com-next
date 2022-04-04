433b25f36b24e8c758dcb790e40707333a55703a
Kamigami55 <eason@easonchang.com>
2022-04-05 01:02:45 +0800

    Fix i18n build error

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,6 @@
-const { LOCALES, DEFAULT_LOCALE } = require('@/constants/siteMeta')
-
 module.exports = {
   i18n: {
-    locales: LOCALES,
-    defaultLocale: DEFAULT_LOCALE,
+    locales: ['en', 'zh-TW'],
+    defaultLocale: 'zh-TW',
   },
 }
