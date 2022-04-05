7ceb93ab2be2e5112df31854cb5b340071a15cb0
Kamigami55 <eason@easonchang.com>
2022-04-05 22:18:41 +0800

    Remove red color from link in post

--- src/components/CustomLink.js
+++ src/components/CustomLink.js
@@ -2,16 +2,9 @@ import Link from 'next/link'
 
 export default function CustomLink({ as, href, ...otherProps }) {
   return (
-    <>
-      <Link as={as} href={href}>
-        {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
-        <a {...otherProps} />
-      </Link>
-      <style jsx>{`
-        a {
-          color: tomato;
-        }
-      `}</style>
-    </>
+    <Link as={as} href={href}>
+      {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
+      <a {...otherProps} />
+    </Link>
   )
 }
