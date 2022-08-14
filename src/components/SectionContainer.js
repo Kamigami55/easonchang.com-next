b821497a645490cc699b1ba3ee0f0a36d852d22c
Eason Chang <eason@easonchang.com>
2022-08-14 13:46:23 +0800

    UI: fix post overflow x bug

--- src/components/SectionContainer.js
+++ src/components/SectionContainer.js
@@ -1,3 +1,7 @@
 export default function SectionContainer({ children }) {
-  return <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
+  return (
+    <div className="mx-auto max-w-full px-4 sm:max-w-3xl sm:px-6 xl:max-w-5xl xl:px-0">
+      {children}
+    </div>
+  )
 }
