b05171628b970a4d850c609eb27653e02b36f25c
Eason Chang <eason@easonchang.com>
2022-08-15 20:53:11 +0800

    Fix post body UI bug

--- src/components/SectionContainer.js
+++ src/components/SectionContainer.js
@@ -1,6 +1,6 @@
 export default function SectionContainer({ children }) {
   return (
-    <div className="mx-auto max-w-full px-4 sm:max-w-3xl sm:px-6 xl:max-w-5xl xl:px-0">
+    <div className="mx-auto max-w-full px-4 sm:px-6 md:max-w-3xl xl:max-w-5xl xl:px-0">
       {children}
     </div>
   )
