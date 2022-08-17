a69d65a1296398ad09f0c03f0b4c36ef7358ce8f
Eason Chang <eason@easonchang.com>
2022-08-18 00:54:33 +0800

    Update AboutPage UI & Add comment to it

--- src/layouts/AuthorLayout.js
+++ src/layouts/AuthorLayout.js
@@ -1,4 +1,7 @@
+import Giscus from '@giscus/react'
+import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
+import { useTheme } from 'next-themes'
 
 import PostBody from '@/components/organisms/PostBody'
 import { PageSEO } from '@/components/SEO'
@@ -6,6 +9,8 @@ import siteMetadata from '@/data/siteMetadata'
 
 export default function AuthorLayout({ children }) {
   const { t } = useTranslation(['common'])
+  const { locale } = useRouter()
+  const { theme } = useTheme()
 
   return (
     <>
@@ -14,15 +19,32 @@ export default function AuthorLayout({ children }) {
         description={t('about-me-description')}
       />
 
-      <div>
+      <div className="mx-auto max-w-prose">
         <div className="space-y-2 pt-12 pb-6 md:space-y-5">
           <h1 className="text-4xl font-extrabold leading-9 tracking-tight text-gray-900 transition-colors dark:text-gray-100 sm:leading-10 md:text-6xl md:leading-14">
             {t('about-me')}
           </h1>
         </div>
+        <div className="space-y-4 divide-y divide-gray-200 transition-colors dark:divide-gray-700">
+          <div className="py-8">
+            <PostBody>{children}</PostBody>
+          </div>
 
-        <div className="items-start space-y-2 py-8">
-          <PostBody className="max-w-none">{children}</PostBody>
+          <div className="py-8">
+            <Giscus
+              repo={siteMetadata.giscusConfig.repo}
+              repoId={siteMetadata.giscusConfig.repoId}
+              category={siteMetadata.giscusConfig.category}
+              categoryId={siteMetadata.giscusConfig.categoryId}
+              mapping="pathname"
+              reactionsEnabled="1"
+              emitMetadata="0"
+              inputPosition="top"
+              theme={theme === 'dark' ? 'transparent_dark' : 'light'}
+              lang={locale}
+              loading="lazy"
+            />
+          </div>
         </div>
       </div>
     </>
