fed5706db0d1a5b1b2782d763cee77495eb89b19
Eason Chang <eason@easonchang.com>
2022-08-17 23:18:35 +0800

    Update about page

--- src/layouts/AuthorLayout.js
+++ src/layouts/AuthorLayout.js
@@ -1,41 +1,27 @@
-// import Image from '@/components/Image'
+import { useTranslation } from 'next-i18next'
+
 import PostBody from '@/components/organisms/PostBody'
 import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
-// import SocialIcon from '@/components/social-icons'
 
 export default function AuthorLayout({ children }) {
+  const { t } = useTranslation(['common'])
+
   return (
     <>
       <PageSEO
-        title={`About - ${siteMetadata.title}`}
-        description={`About me - ${siteMetadata.author}`}
+        title={`${t('about-me')} - ${siteMetadata.title}`}
+        description={t('about-me-description')}
       />
-      <div className="divide-y">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 transition-colors dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
+
+      <div>
+        <div className="space-y-2 pt-12 pb-6 md:space-y-5">
+          <h1 className="text-4xl font-extrabold leading-9 tracking-tight text-gray-900 transition-colors dark:text-gray-100 sm:leading-10 md:text-6xl md:leading-14">
+            {t('about-me')}
           </h1>
         </div>
+
         <div className="items-start space-y-2 py-8">
-          {/* <div className="flex flex-col items-center space-x-2 pt-8">
-            <Image
-              src={avatar}
-              alt="avatar"
-              width="192px"
-              height="192px"
-              className="h-48 w-48 rounded-full"
-            />
-            <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="flex space-x-3 pt-6">
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
-              <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
-            </div>
-          </div> */}
           <PostBody className="max-w-none">{children}</PostBody>
         </div>
       </div>
