64c59ce89d872f5594437774b3ecbf4df8ea5941
Eason Chang <eason@easonchang.com>
2022-08-26 21:28:53 +0800

    Install @tailwindcss/line-clamp
    For line-clamp className of later usage
    
    ref:
    https://tailwindcss.com/blog/multi-line-truncation-with-tailwindcss-line-clamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -156,5 +156,8 @@ module.exports = {
       }),
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+  ],
 };
