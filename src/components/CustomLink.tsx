013c0bfe3ef1c4c32cec9de260c8b778735e5a44
Eason Chang <eason@easonchang.com>
2023-02-05 13:50:30 +0800

    Run codemod for Next.js 13 new Link
    ` npx @next/codemod@latest new-link src`
    
    https://nextjs.org/docs/advanced-features/codemods#new-link

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -4,23 +4,31 @@ import Link from 'next/link';
 
 type Props = React.ComponentPropsWithoutRef<'a'>;
 
-const CustomLink = ({ href, ...rest }: Props) => {
+const CustomLink = ({ href, children, ...rest }: Props) => {
   const isInternalLink = href && href.startsWith('/');
   const isAnchorLink = href && href.startsWith('#');
 
   if (isInternalLink) {
     return (
-      <Link href={href}>
-        <a {...rest} />
+      <Link href={href} {...rest}>
+        {children}
       </Link>
     );
   }
 
   if (isAnchorLink) {
-    return <a href={href} {...rest} />;
+    return (
+      <a href={href} {...rest}>
+        {children}
+      </a>
+    );
   }
 
-  return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />;
+  return (
+    <a target="_blank" rel="noopener noreferrer" href={href} {...rest}>
+      {children}
+    </a>
+  );
 };
 
 export default CustomLink;
--- src/components/LanguageSwitch.tsx
+++ src/components/LanguageSwitch.tsx
@@ -7,13 +7,13 @@ const LanguageSwitch = () => {
   const nextLocale = router.locale === 'en' ? 'zh-TW' : 'en';
 
   return (
-    <Link locale={nextLocale} href={{ pathname, query }}>
-      <a
-        aria-label="Toggle Language"
-        className="rounded p-2 text-2xl leading-6 transition-colors hover:bg-gray-100 dark:hover:bg-gray-800 sm:p-3"
-      >
-        {router.locale === 'en' ? '🇺🇸' : '🇹🇼'}
-      </a>
+    <Link
+      locale={nextLocale}
+      href={{ pathname, query }}
+      aria-label="Toggle Language"
+      className="rounded p-2 text-2xl leading-6 transition-colors hover:bg-gray-100 dark:hover:bg-gray-800 sm:p-3"
+    >
+      {router.locale === 'en' ? '🇺🇸' : '🇹🇼'}
     </Link>
   );
 };
